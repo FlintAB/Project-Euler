7769d2689ac6559c63067546ee76ba3e97812b31
Aleksey <109232395+FlintAB@users.noreply.github.com>
2025-06-06 22:42:32 +0300

    Update problem-1-multiples-of-3-or-5.js

--- Problems 1 to 100/problem-1-multiples-of-3-or-5.js
+++ Problems 1 to 100/problem-1-multiples-of-3-or-5.js
@@ -1,8 +1,9 @@
 ** start of script.js **
-
+// Сумма всех кратных чисел 3 или 5
 function multiplesOf3Or5(number) {
    let result = 0;
    for (let i = 0; i < number; i++){
+      // Проверка кратности
      if (i % 3 === 0 || i % 5 === 0){
        result+=i;
      }
